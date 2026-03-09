9d97b3fa860cd5042d17d8c492492528696ea795
Shahbaj Siddique <shahbajs593@gmail.com>
2026-03-10 01:47:26 +0530

    Skeleton use

--- front-end/src/skeleton/ProductSkeleton.jsx
+++ front-end/src/skeleton/ProductSkeleton.jsx
@@ -1,15 +1,23 @@
 export default function ProductSkeleton() {
   return (
-    <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 animate-pulse">
-      
-      <div className="bg-gray-300 h-[500px] rounded-xl"></div>
+    <div className="min-w-[150px] sm:min-w-[161px] md:min-w-[180px] bg-white rounded-xl border border-gray-200 p-3 shadow-sm animate-pulse">
 
-      <div className="space-y-4">
-        <div className="bg-gray-300 h-8 w-2/3 rounded"></div>
-        <div className="bg-gray-300 h-6 w-1/4 rounded"></div>
-        <div className="bg-gray-300 h-6 w-1/3 rounded"></div>
-        <div className="bg-gray-300 h-32 rounded"></div>
-        <div className="bg-gray-300 h-12 w-40 rounded"></div>
+      {/* Image */}
+      <div className="sm:h-44 md:h-50 lg:h-54 bg-gray-200 rounded-lg"></div>
+
+      {/* Title */}
+      <div className="mt-3 space-y-2">
+        <div className="h-3 bg-gray-200 rounded w-[90%]"></div>
+        <div className="h-3 bg-gray-200 rounded w-[60%]"></div>
+      </div>
+
+      {/* Category */}
+      <div className="mt-2 h-3 bg-gray-200 rounded w-16"></div>
+
+      {/* Price Section */}
+      <div className="flex justify-between items-center mt-2">
+        <div className="h-4 bg-gray-200 rounded w-14"></div>
+        <div className="h-3 bg-gray-200 rounded w-10"></div>
       </div>
 
     </div>
